2ce512eeea1569406f3d128a51f18dd4adb5b477
VishnuVY2017 <https://vishnuvy2017@github.com>
2025-07-01 17:38:12 -0600

    initial setup with one program

---
+++ src/main/java/com/core/java/dsa/arrays/Search_for_given_Element.java
@@ -0,0 +1,25 @@
+package com.core.java.dsa.arrays;
+
+public class Search_for_given_Element {
+
+    private static int linearSearchFun(int[] arr, int element) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == element) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {25, 10, 5, 15, 30, 20, 50};
+        int element = 90;
+        int index = linearSearchFun(arr, element);
+        if (index == -1)
+            System.out.println("Element Not Found");
+        else
+            System.out.println("Element Found at Index : " + index);
+    }
+}
