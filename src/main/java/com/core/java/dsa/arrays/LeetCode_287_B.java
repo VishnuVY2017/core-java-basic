f568a385e6454c52dd9691e5b5f135f94f036180
VishnuVY2017 <https://vishnuvy2017@github.com>
2025-07-03 23:42:51 -0600

    leedt code solution

---
+++ src/main/java/com/core/java/dsa/arrays/LeetCode_287_B.java
@@ -0,0 +1,34 @@
+package com.core.java.dsa.arrays;
+
+import java.util.Arrays;
+
+// 287. Find the Duplicate Number
+public class LeetCode_287_B {
+
+    public static int containsDuplicate(int[] arr) {
+
+        Arrays.sort(arr);
+        int n =arr.length-1;
+        for(int i = 0; i<=n; i++){
+            if(arr[i] == arr[i+1]){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+
+        int arr[] = {10, 20, 30, 40, 50, 50};
+        System.out.println(Arrays.toString(arr));
+
+        int b = containsDuplicate(arr);
+        System.out.println( b);
+    }
+}
+
+/*
+// Time Complexity - O(n^2) // not the good code.
+// Aux Space - O(1)
+ */
